f9486c62b992e9811ed9d082e20ca35e92fc73b3
FrederikVA <s224308@student.dtu.dk>
2024-09-27 11:34:23 +0200

    More tests!
    all passes

--- Assignment_1/test.sh
+++ Assignment_1/test.sh
@@ -4,15 +4,33 @@
 
 in="abbabaq"
 out="0,3,5;"
-
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
 in="abccbaabcq"
 out="5;"
-
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
 in=""
 out=";"
-
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="q"
+out=";"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="qaba"
+out=";"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="aaaq"
+out="0,1,2;"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="accq"
+out=";"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="bbq"
+out=";"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
